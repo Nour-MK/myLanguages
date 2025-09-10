c889145deff8f0d3ac90967d6678034e5a6c33d7
Nour Mostafa <123479685+Nour-MK@users.noreply.github.com>
2025-09-10 21:59:43 +0400

    Add files via upload

---
+++ Scripting Languages/Shell Scripting/cmd/1_DisplayCurrentUser.bat
@@ -0,0 +1,4 @@
+whoami
+REM this command used to display the current user and group information
+pause
+REM this command keeps the Command Prompt window open after the script finishes executing, allowing you to see the output
---
+++ Scripting Languages/Shell Scripting/cmd/2_CommentsAndHidingCommands.bat
@@ -0,0 +1,8 @@
+@echo off
+REM this commands prevents the command line tool from showing the commands you wrote (and the comments) on the terminal again then the output - it will just give you the output to your command
+
+whoami & REM you can put a comment on the same line as the command like this
+REM or you can put a comment on a separate line like this
+REM note that when you place a comment on the same line as the command you will need the & operator whose function is to basically concatenate batch commands on the same line
+
+pause
